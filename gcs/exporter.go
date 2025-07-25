7e9a811825c6a27dcec902047c9f2e6571382385
Omar Polo <op@omarpolo.com>
2025-07-25 16:06:25 +0000

    exporter: allow to export to a sub-prefix

--- gcs/exporter.go
+++ gcs/exporter.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"strings"
+	"path"
 
 	"cloud.google.com/go/storage"
 	"github.com/PlakarKorp/kloset/objects"
@@ -24,7 +25,7 @@ func NewExporter(ctx context.Context, opts *exporter.Options, proto string, para
 	target := params["location"]
 	bucket, path, _ := strings.Cut(strings.TrimPrefix(target, proto+"://"), "/")
 
-	path = strings.TrimPrefix(path, "/")
+	path = strings.Trim(path, "/")
 
 	client, err := storage.NewClient(ctx)
 	if err != nil {
@@ -41,12 +42,14 @@ func NewExporter(ctx context.Context, opts *exporter.Options, proto string, para
 	}, nil
 }
 
+func (g *gcsExporter) realpath(rel string) string { return path.Join(g.path, rel) }
+
 func (g *gcsExporter) Root() string { return g.path }
 
 func (g *gcsExporter) CreateDirectory(pathname string) error { return nil }
 
 func (g *gcsExporter) StoreFile(pathname string, fp io.Reader, size int64) error {
-	pathname = strings.TrimLeft(pathname, "/")
+	pathname = g.realpath(strings.TrimLeft(pathname, "/"))
 
 	w := g.bucket.Object(pathname).NewWriter(g.ctx)
 	if _, err := io.Copy(w, fp); err != nil {
