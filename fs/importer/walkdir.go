9c59eef6492e21ad8495b7d0fe1f2710df74cf06
Omar Polo <op@omarpolo.com>
2026-02-26 10:54:43 +0000

    simplify ranging over jobs

--- fs/importer/walkdir.go
+++ fs/importer/walkdir.go
@@ -36,19 +36,7 @@ import (
 func (f *FSImporter) walkDir_worker(ctx context.Context, jobs <-chan file, records chan<- *connectors.Record, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for {
-		var (
-			p  file
-			ok bool
-		)
-
-		select {
-		case p, ok = <-jobs:
-			if !ok {
-				return
-			}
-		}
-
+	for p := range jobs {
 		// fixup the rootdir if it happened to be a file
 		if !p.info.IsDir() && p.path == f.rootDir {
 			f.rootDir = filepath.Dir(f.rootDir)
