2465ba8c8f4dc5c043a53452d3469c8544b14200
Omar Polo <op@omarpolo.com>
2026-03-26 17:07:56 +0000

    unbreak build
    during rebase that header snuck in

--- k8s/csi.go
+++ k8s/csi.go
@@ -20,7 +20,6 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/apimachinery/pkg/watch"
